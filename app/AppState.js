35bb9e1ddd59890a7e186ce62ea193297c01850e
Nikolas Clark <nac09@msn.com>
2025-06-04 14:41:48 -0700

    House Controller, House Model, new houses in AppState

--- app/AppState.js
+++ app/AppState.js
@@ -1,4 +1,5 @@
 import { Car } from './models/Car.js'
+import { House } from './models/House.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
@@ -52,6 +53,27 @@ class ObservableAppState extends EventEmitter {
         listedAt: '6/8/2024'
       }
     ),
+  ]
+
+  houses = [
+    new House({
+      year: '1974',
+      bedrooms: 3,
+      bathrooms: 2.5,
+      sqft: 1400,
+      price: 375000,
+      description: 'Newly renovated mid-century modern home',
+      imgURL: 'https://images.unsplash.com/photo-1570129477492-45c003edd2be?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8SG91c2V8ZW58MHx8MHx8fDA%3D'
+    }),
+    new House({
+      year: '1945',
+      bedrooms: 3,
+      bathrooms: 2,
+      sqft: 1100,
+      price: 175000,
+      description: 'Recently refurbished post war home, ready for a new family',
+      imgURL: 'https://plus.unsplash.com/premium_photo-1733760125038-06564d0a4568?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8MTk0MHMlMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D'
+    })
   ]
 }
 
