74905248eda0fd3412cd4b10f35f22a07e988690
Nikolas Clark <nac09@msn.com>
2025-06-04 16:50:26 -0700

    Added House Form

--- app/controllers/HouseController.js
+++ app/controllers/HouseController.js
@@ -3,7 +3,27 @@ import { AppState } from "../AppState.js";
 export class HouseController {
   constructor() {
     // console.log("House Controller is here!");
+    this.drawHouses()
+    // AppState.on('houses', this.drawHouses)
+
   }
 
+  drawHouses() {
+    const houses = AppState.houses
+    let houseListingContent = ''
+    houses.forEach(house => houseListingContent += house.listingHTMLTemplate)
+    const houseListingsElm = document.getElementById('house-listings')
+    // console.log('drawing houses', houseListingsElm);
+    houseListingsElm.innerHTML = houseListingContent
+  }
 
+  createHouse() {
+    event.preventDefault()
+    let form = event.target
+    let houseData = {
+
+    }
+    console.log(houseData);
+
+  }
 }
--- app/models/House.js
+++ app/models/House.js
@@ -11,4 +11,32 @@ export class House {
     this.description = data.description
     this.imgURL = data.imgURL
   }
+
+  get listingHTMLTemplate() {
+    return `
+    <div class="col-md-6 mb-3">
+              <div class="position-relative shadow house-border">
+                <img
+                  src="${this.imgURL}"
+                  alt="An image of a house" class="house-img">
+                <span class="house-price bg-dark text-success fs-2 px-2 d-inline-block">
+                  $${this.priceWithCommas}
+                </span>
+                <div class="p-3">
+                  <div class="d-flex justify-content-between">
+                    <h3>${this.year}</h3>
+                  </div>
+                  <p>${this.description}</p>
+                  <p>${this.bedrooms} bedrooms</p>
+                  <p>${this.bathrooms} bathrooms</p>
+                  <p>${this.sqft} sqft</p>
+                </div>
+              </div>
+            </div>
+            `
+  }
+
+  get priceWithCommas() {
+    return this.price.toLocaleString()
+  }
 }
